8fe02ab8a5ada4cd70c5c334c191499b1dceac6a
Fahmin <fehmin.shamilli@azerimed.com>
2022-02-14 15:17:31 +0400

    Create Product factory and seeder

---
+++ restapi/database/seeders/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->truncate();
+        DB::table('products')->insert([
+            'name' =>'Car',
+            'slug'=>'car',
+            'description'=>'hello some',
+            'price'=> 19.8
+        ]);
+        Product::factory(10)->create();
+    }
+}
